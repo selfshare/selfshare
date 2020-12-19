9b6743628f6b2202f2e2f8ce091d7e459afbe488
Max Ermackov <max@emx-tech.com>
2020-12-19 22:13:51 +0100

    added some e2e tests

--- src/frontend/e2e/src/app.e2e-spec.ts
+++ src/frontend/e2e/src/app.e2e-spec.ts
@@ -1,16 +1,53 @@
 import { AppPage } from './app.po';
-import { browser, logging } from 'protractor';
+import {browser, logging, protractor} from 'protractor';
 
-describe('workspace-project App', () => {
+describe('selfshare frontend', () => {
   let page: AppPage;
 
   beforeEach(() => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getTitleText()).toEqual('frontend app is running!');
+  it('should display header buttons', () => {
+    page.navigateTo('');
+    expect(page.getHeaderButtonLink()).toEqual('/');
+  });
+
+  it('should display home page', () => {
+    page.navigateTo('');
+    expect(page.getGalleryText()).toEqual('none');
+  });
+
+  it('should display about page', () => {
+    page.navigateTo('about');
+    expect(page.getAboutImage()).toEqual('profile image');
+  });
+
+  it('should display disclaimer page', () => {
+    page.navigateTo('disclaimer');
+    expect(page.getDisclaimerTitle()).toMatch(/disclaimer/i);
+  });
+
+  it('should display login page', () => {
+    page.navigateTo('login');
+    expect(page.getLoginTitle()).toMatch(/login/i);
+  });
+
+  it('should display login error on empty input', () => {
+    page.navigateTo('login');
+    page.clickLoginButton();
+    expect(page.getLoginErrorText()).toEqual('The credentials are wrong. Please try again!');
+  });
+
+  it('should display login error on short password', () => {
+    page.navigateTo('login');
+    page.fillLoginCredentials();
+    page.clickLoginButton();
+    expect(page.getLoginErrorText()).toEqual('The credentials are wrong. Please try again!');
+  });
+
+  it('should display dashboard page on successful login', () => {
+
   });
 
   afterEach(async () => {
--- src/frontend/e2e/src/app.po.ts
+++ src/frontend/e2e/src/app.po.ts
@@ -1,11 +1,41 @@
-import { browser, by, element } from 'protractor';
+import {browser, by, element, ElementFinder} from 'protractor';
 
 export class AppPage {
-  navigateTo(): Promise<unknown> {
-    return browser.get(browser.baseUrl) as Promise<unknown>;
+  navigateTo(path: string): Promise<unknown> {
+    return browser.get(browser.baseUrl + path) as Promise<unknown>;
   }
 
-  getTitleText(): Promise<string> {
-    return element(by.css('app-root .content span')).getText() as Promise<string>;
+  getGalleryText(): Promise<string> {
+    return element(by.css('#loading-spinner')).getCssValue('display') as Promise<string>;
+  }
+
+  getHeaderButtonLink(): Promise<string> {
+    return element(by.css('.active a')).getAttribute('routerlink') as Promise<string>;
+  }
+
+  getAboutImage(): Promise<string> {
+    return element(by.css('app-about img')).getAttribute('alt') as Promise<string>;
+  }
+
+  getDisclaimerTitle(): Promise<string> {
+    return element(by.css('h1')).getText() as Promise<string>;
+  }
+
+  getLoginTitle(): Promise<string> {
+    return element(by.css('h4')).getText() as Promise<string>;
+  }
+
+  fillLoginCredentials(): void{
+    element(by.css('#inputUsername')).sendKeys('admin');
+    element(by.css('#inputPassword')).sendKeys('1234');
+  }
+
+
+  clickLoginButton(): void {
+    element(by.css('[value="Login"]')).click();
+  }
+
+  getLoginErrorText(): Promise<string> {
+    return element(by.css('.text-danger')).getText() as Promise<string>;
   }
 }
